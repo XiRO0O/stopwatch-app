bade4c3002a7337a63af6ae517da8893aaea2627
ONKJUNUA <jaseko2002@gmail.com>
2022-06-18 16:45:21 +0200

    reset/stop function

--- code.py
+++ code.py
@@ -6,8 +6,8 @@ layout = [
     [sg.Push(), sg.Image ('x.png', pad = 0, enable_events = True, key = '-CLOSE-')],
     [sg.VPush()],
     [sg.Text('TIME', font = 'Impact 100', key = '-TIME-')],
-    [sg.Button('Start', button_color = ('#FFFFFF','#666666'), border_width = 0, key = '-START-'),
-    sg.Button('Lap', button_color = ('#FFFFFF','#666666'), border_width = 0, key = '-LAP-')],
+    [sg.Button('Start', button_color = ('#FFFFFF','#666666'), font = 'Impact 15', border_width = 0, key = '-START-'),
+    sg.Button('Lap', button_color = ('#FFFFFF','#666666'), font = 'Impact 15', border_width = 0, key = '-LAP-', visible = False)],
     [sg.VPush()]]
 
 window = sg.Window(
@@ -25,8 +25,16 @@ while True:
         break
 
     if event == '-START-':
-        start_time = time()
-        active = True
+        if active:
+            active = False
+            window['-START-'].update('Reset')
+            window['-LAP-'].update(visible = False)
+
+        else:
+            start_time = time()
+            active = True
+            window['-START-'].update('Stop')
+            window['-LAP-'].update(visible = True)
 
     if active:
         elapsed_time = round(time() - start_time,1)
