d01e62dc181af01e642976aeb7fa2cca8b49e605
ONKJUNUA <jaseko2002@gmail.com>
2022-06-18 16:53:36 +0200

    full reset

--- code.py
+++ code.py
@@ -1,21 +1,23 @@
 import PySimpleGUI as sg
 from time import time
 
-sg.theme('Black')
-layout = [
-    [sg.Push(), sg.Image ('x.png', pad = 0, enable_events = True, key = '-CLOSE-')],
-    [sg.VPush()],
-    [sg.Text('TIME', font = 'Impact 100', key = '-TIME-')],
-    [sg.Button('Start', button_color = ('#FFFFFF','#666666'), font = 'Impact 15', border_width = 0, key = '-START-'),
-    sg.Button('Lap', button_color = ('#FFFFFF','#666666'), font = 'Impact 15', border_width = 0, key = '-LAP-', visible = False)],
-    [sg.VPush()]]
+def create_window():
+    sg.theme('Black')
+    layout = [
+        [sg.Push(), sg.Image ('x.png', pad = 0, enable_events = True, key = '-CLOSE-')],
+        [sg.VPush()],
+        [sg.Text('TIME', font = 'Impact 100', key = '-TIME-')],
+        [sg.Button('Start', button_color = ('#FFFFFF','#666666'), font = 'Impact 15', border_width = 0, key = '-START-'),
+        sg.Button('Lap', button_color = ('#FFFFFF','#666666'), font = 'Impact 15', border_width = 0, key = '-LAP-', visible = False)],
+        [sg.VPush()]]
+    return sg.Window(
+        'StopWatch',
+        layout,
+        size = (400,400),
+        no_titlebar = True,
+        element_justification = 'center')
 
-window = sg.Window(
-    'StopWatch',
-    layout,
-    size = (400,400),
-    no_titlebar = True,
-    element_justification = 'center')
+window = create_window()
 start_time = 0
 active = False
 
@@ -29,12 +31,16 @@ while True:
             active = False
             window['-START-'].update('Reset')
             window['-LAP-'].update(visible = False)
-
         else:
-            start_time = time()
-            active = True
-            window['-START-'].update('Stop')
-            window['-LAP-'].update(visible = True)
+            if start_time > 0:
+                window.close()
+                window = create_window()
+                start_time = 0
+            else:
+                start_time = time()
+                active = True
+                window['-START-'].update('Stop')
+                window['-LAP-'].update(visible = True)
 
     if active:
         elapsed_time = round(time() - start_time,1)
