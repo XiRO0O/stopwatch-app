637ccf59596685d0b7c43c05c249f24f4541faf0
ONKJUNUA <jaseko2002@gmail.com>
2022-06-18 16:23:03 +0200

    just upgrading

--- code.py
+++ code.py
@@ -2,22 +2,24 @@ import PySimpleGUI as sg
 
 sg.theme('Black')
 layout = [
+    [sg.Push(), sg.Image ('x.png', pad = 0, enable_events = True, key = '-CLOSE-')],
     [sg.VPush()],
-    [sg.Text('TIME')],
-    [sg.Button('Start'),sg.Button('Lap')],
+    [sg.Text('TIME', font = 'Impact 100')],
+    [sg.Button('Start', button_color = '#FF0000', border_width = 0),
+    sg.Button('Lap', button_color = '#FF0000', border_width = 0)],
     [sg.VPush()]
 ]
 
 window = sg.Window(
     'StopWatch',
     layout,
-    size = (500,500),
+    size = (400,400),
     no_titlebar = True,
     element_justification = 'center')
 
 while True:
     event, values = window.read()
-    if event in (sg.WIN_CLOSED, 'Start'):
+    if event in (sg.WIN_CLOSED, '-CLOSE-'):
         break
 
 window.close()
